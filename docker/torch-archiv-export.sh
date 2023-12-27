c51fc7cbcb96255e5938bce5e20f1e56f33d4f01
supermomo668 <mymomo119966.mm@gmail.com>
2023-12-27 02:23:13 +0000

    updated export func

--- docker/torch-archiv-export.sh
+++ docker/torch-archiv-export.sh
@@ -2,6 +2,7 @@
 Help() {
   echo "export model from .pth to .mar in the same directory."
   echo "Arg 1    : Name of directory of the location of model in checkpoint directory "
+  echo "[ELEVATED permission]: sudo -u mmym_ezout_gmail_com env PATH=$PATH bash torch-archiv-export.sh  [args]"
   
 }
 # HARD-CODED
@@ -16,7 +17,7 @@ VERSION=${4:-1.0.0}
 # script
 MODEL_PATH=$MODEL_CKPTS/$MODEL/$ckpt_file
 mkdir -p $EXPORT_PATH
-
+chmod 600 $EXPORT_PATH
 echo "Creating model archiv from:..."
 echo "    $MODEL_CKPTS"
 echo "    $HANDLER"
